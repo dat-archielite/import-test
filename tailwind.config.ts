029538733db60b3d88defa6f11a77429c802b02e
Ngo Quoc Dat <dat@archielite.com>
2023-04-12 23:47:46 +0700

    Update tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config
+
